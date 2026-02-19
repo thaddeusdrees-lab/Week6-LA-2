4089a0db94868806f7d33cbef1d4cb56b9c60ea2
thaddeusdrees-lab <thaddeusdrees@gmail.com>
2026-02-18 22:49:05 -0500

    Implement performance timing for fpgrowth and apriori
    Added timing measurements for fpgrowth and apriori functions.

--- FP_growth.py
+++ FP_growth.py
@@ -1,6 +1,9 @@
+
 import pandas as pd
 import mlxtend
+import time
 from mlxtend.frequent_patterns import fpgrowth
+from mlxtend.frequent_patterns import apriori
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import association_rules
 
@@ -8,8 +11,16 @@ dataset = [['M','O','N','K','E','Y'],['D','O','N','K','E','Y'],['M','A','K','E']
 te = TransactionEncoder()
 te_ary = te.fit(dataset).transform(dataset)
 df = pd.DataFrame(te_ary, columns=te.columns_)
-df
+
+start = time.perf_counter()
 stuff = fpgrowth(df, min_support=0.6, use_colnames=True)
+end = time.perf_counter()
+print(end-start)
+start = time.perf_counter()
+things =apriori(df, min_support=0.6, use_colnames=True)
+end = time.perf_counter()
+print(end-start)
+print(things,stuff)
 table  = (association_rules(stuff, min_threshold=0.6))
 print(table[(table['confidence'] != 1.00)])
 print(table)
